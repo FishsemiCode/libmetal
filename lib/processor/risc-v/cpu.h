72dc97e05ef3dffc127393c3b015bf851451b517
anchao <anchao@pinecone.net>
2018-12-13 12:09:24 +0800

    lib: processor: add risc-v support
    Change-Id: Iee9c6111e739773ce468f8770cd546c59857e11e
    Signed-off-by: anchao <anchao@pinecone.net>

---
+++ lib/processor/risc-v/cpu.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018, Pinecone Inc. and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * @file	cpu.h
+ * @brief	CPU specific primatives
+ */
+
+#ifndef __METAL_RISC_V_CPU__H__
+#define __METAL_RISC_V_CPU__H__
+
+#define metal_cpu_yield()
+
+#endif /* __METAL_RISC_V_CPU__H__ */
